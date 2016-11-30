c3db38325d07ecd5571e5323357b6805001e26fd
John Franco Saraceno <saraceno@usgs.gov>
2016-11-30 11:11:00 -0800

    Updated setup.py.1)Removed prepackaged python libraries form setup_requires arg.2) Fixed pakage name.3)removed bad link to cimis package

--- setup.py
+++ setup.py
@@ -10,13 +10,13 @@ with open("README.md") as readme_file:
 with open("LICENSE") as license_file:
     license = license_file.read()
 
-setup(name='suna_proc',
+setup(name='suna_process',
       description='Python package to process Satlantic SUNA v2 data files',
       author='John Franco Saraceno',
       author_email='saraceno@usgs.gov',
-      url='https://bitbucket.org/geofranco/suna_proc',
+      url='https://bitbucket.org/geofranco/suna_process',
       version='1.0',
-      packages=['cimis'],
+      packages=[suna_process],
       install_requires=['datetime', 'glob', 'matplotlib', 'numpy',
 						'pandas', 'statsmodels'],
       license=license,
