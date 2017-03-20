de65e26e5368f7f0733dcb02e4c0f6666f27dea0
John Franco Saraceno <OneGneissGuy@users.noreply.github.com>
2017-03-20 13:58:24 -0700

    fixes example typos

--- examples/iws_suna_proc.py
+++ examples/iws_suna_proc.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-:DESCRIPTION:NAWQA IWS nitate file processor
+:DESCRIPTION:internally logged daily suna file processor
 This script processes SUNA version 2 full_ascii log data file located in a
 directory
 
@@ -27,14 +27,14 @@ from suna_process import process
 
 def main(file_direc, fmatch, suna_version, interval,
          UTC_tz_offset, datetime_name):
-    # set the ouTfile name based on the name of the file directory
+    # set the outfile name based on the name of the file directory
     no3_mad_raw = process(file_direc, fmatch, suna_version, interval,
                           UTC_tz_offset, datetime_name)
     # filter out NO3 conc data when sensor in air (typically, NO3 < 0.1 mg/L),
     # there is an error (NO3 = -1), or concentration exceeds concentration
     # limit
     no3_mad = no3_mad_raw[(no3_mad_raw > 0.1) & (no3_mad_raw < 58)]
-    # write mad proced nitrata concentration output to csv file
+    # write mad processed nitrate concentration output to csv file
     no3_mad.to_csv(os.path.join(file_direc, (out_name + '_SUNA_NO3_mad.csv')))
 
     # plot the raw and mad processed nitrate concentration timeseries data
@@ -49,11 +49,11 @@ if __name__ == "__main__":
     # ============================BEGIN USER VARIABLES=========================
     # DEFINE FILE CONTAINING DIRECTORY
     file_direc = r'C:\Users\saraceno\Documents\Code\Python\repos\suna_proc\examples\example_data'
-    fmatch = 'D*.csv'  # match and process all daily log files
+    fmatch = 'D*.csv'  # match and process all daily csv files
     out_name = file_direc.split(os.sep)[-1]
     suna_version = 2
     # define sampling interval
-    interval = 30  # in minutes
+    interval = 15  # in minutes
     # define timezone offset relative to UTC, in hours
     UTC_tz_offset = -5
     # name datetime index based on time zone
